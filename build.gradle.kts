81f5a4b17f786b94a3e98191a1d6d25db1f65d0c
Erik Andreas Barreto de Vera <erik.dvera+dev@proton.me>
2024-11-18 21:19:13 +0000

    Add PostgreSQL support with new Products entity
    Introduced a `Products` entity class and configured PostgreSQL integration in the project. Updated `build.gradle.kts` to include Spring Data JPA and PostgreSQL dependencies. Added `.env`, `compose.yml`, and `application.properties` to set up PostgreSQL and PgAdmin configurations.

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,9 +21,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     runtimeOnly("io.micrometer:micrometer-registry-prometheus")
     runtimeOnly("org.springframework.boot:spring-boot-docker-compose")
+    runtimeOnly("org.postgresql:postgresql")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
---
+++ src/main/java/com/erikandreas/metricsdemo/Products.java
@@ -0,0 +1,18 @@
+package com.erikandreas.metricsdemo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Products {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Double price;
+}
